dd0c904b790822d6de7907ea1c6238841ec622ec
legalgeek <legalgeek@mbp>
2022-03-27 11:35:44 +0800

    init

---
+++ src/views/NameView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data(){
+    return{
+      msg:"hello minglei"
+    }
+  }
+  // component options
+}
+</script>
+
+<template>
+  <h1>{{msg.split("").reverse().join("")}}</h1>
+</template>
+
+<style>
+/* h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+} */
+</style>
